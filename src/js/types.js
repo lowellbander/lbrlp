2b2279b6746dc4b9f22d6e921aa9cbb36e139a2c
Lowell Bander <lowellbander@gmail.com>
2019-08-21 08:18:07 -0700

    actual Flow support (not native VS Code typescript support

---
+++ src/js/types.js
@@ -0,0 +1,4 @@
+type Tier = 'Tier 1'| 'Tier 2' | 'Tier 3' | 'PT';
+type Volume = {sets: number, reps: number, intensity: number, isAmrap: boolean};
+type Lift = 'Squat' | 'Overhead Press' | 'Pull Ups' | 'Dumbell Overhead Press' | 'Deadlift' | 'Bench' | 'Row' | 'Lateral Raise' | 'Internal Shoulder Rotation' | 'External Shoulder Rotation';
+type Workout = {[Tier]: $ReadOnlyArray<Lift>};
