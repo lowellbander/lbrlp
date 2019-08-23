deeafc3dc1224f052d8f4e757d3a93bea7c29ccd
Lowell Bander <lowellbander@gmail.com>
2019-08-22 21:51:57 -0700

    view source on github link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ const {Program} = require('./Program.js');
 
 export function App() {
   return (
-    <Program />
+    <div>
+      <div>
+        <a href="https://github.com/lowellbander/lbrlp/tree/master/src"> View source on Github </a>
+      </div>
+      <Program />
+    </div>
   );
 }
