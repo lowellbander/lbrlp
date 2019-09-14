62f4144da014914309a15ea2d1632cbd16f3dee0
Lowell Bander <lowellbander@gmail.com>
2019-09-13 23:14:56 -0700

    update 1RMs

--- src/oneRepMaxes.js
+++ src/oneRepMaxes.js
@@ -3,16 +3,16 @@
 import type {Lift} from './types.js'
 
 const oneRepMaxes : {[Lift]: number} = {
-    'Squat': 68,
-    'Overhead Press': 56,
+    'Squat': 90,
+    'Overhead Press': 60,
     'Pull Ups': 0, 
     'Dumbell Overhead Press': 25, 
-    'Deadlift': 96,
-    'Bench': 68,
-    'Row': 106,
-    'Lateral Raise': 13,
-    'Internal Shoulder Rotation': 25,
-    'External Shoulder Rotation': 25,
+    'Deadlift': 188,
+    'Bench': 115,
+    'Row': 125,
+    'Lateral Raise': 21,
+    'Internal Shoulder Rotation': 20,
+    'External Shoulder Rotation': 20,
 };
 
 export function getOneRepMax(lift: Lift): number {
